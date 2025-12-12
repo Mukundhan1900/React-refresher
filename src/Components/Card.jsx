1085c7cd5d4190d79357312ff6c7f12427b231c8
Mukundhan M <mukundhan@Mukundhans-MacBook-Air.local>
2025-12-13 01:44:38 +0530

    color fix

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,9 +6,9 @@ function Card(props){
     return (
     <>
      <div>
-        <h2>{props.name}</h2>
-        <h5>{props.type}</h5>
-        <p>{props.desc}</p>
+        <h2 className='color-1'>{props.name}</h2>
+        <h5 className='color-2'>{props.type}</h5>
+        <p className='color-3'>{props.desc}</p>
      </div>
     </>
   )
